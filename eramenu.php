c006b17d36c47f552764cb82caa8f885af0cbae9
= <=>
2021-03-08 08:55:53 -0600

    Pendiente los menus

---
+++ eramenu.php
@@ -0,0 +1,27 @@
+
+ <!-- Navegation Menu -->
+ <nav class="green darken-4 navbar-fixed" role="navigation">
+    <div class="nav-wrapper container nav">
+        <a href="#" class="brand-logo">
+            <img src="img/logo.png" alt="">
+        </a>
+
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+      <ul class="right hide-on-med-and-down">
+          <li><a href="#" class= "white-text flow-text">Inicio</a></li>
+          <li><a href="#" class= "white-text flow-text">Quienes somos</a></li>
+          <li><a href="#" class= "white-text flow-text">Maquinaria</a></li>
+        <!--<li><a href="#" class="btn white black-text waves-effect waves-blue-grey lighten-1">Contacto</a></li> -->
+        <li><a href="#" class="white-text flow-text">Contacto</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#" class="white-text">Quienes somos</a></li>
+        <li><a href="#" class="white-text">Maquinaria</a></li>
+        <li><a href="#" class="white-text" >Contacto</a></li>
+      </ul>
+      
+    </div>
+  </nav>
